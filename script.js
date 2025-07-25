1d32ef8d116512b1973ba0ff64c89819bffdcd4e
Suhani <suhaninegi124@gmail.com>
2025-07-25 19:17:24 +0530

    theme function added

---
+++ script.js
@@ -0,0 +1,25 @@
+const theme=document.getElementById("theme-toggle");
+const body=document.body;
+
+let isLight=true;
+theme.addEventListener("click",()=>{
+    const icon = document.getElementById("icon");
+
+    icon.style.opacity = "0"; // fade out
+
+    setTimeout(() => {
+        if (isLight) {
+            body.setAttribute("class", "darktheme");
+            isLight = false;
+            icon.classList.remove("fa-moon");
+            icon.classList.add("fa-sun");
+        } else {
+            body.setAttribute("class", "lighttheme");
+            isLight = true;
+            icon.classList.remove("fa-sun");
+            icon.classList.add("fa-moon");
+        }
+
+        icon.style.opacity = "1"; // fade in
+    }, 150); // wait a bit before fading in new icon
+})
