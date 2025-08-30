53bd66e721fd84be2b0e4a410479a1c4d3776a7a
Suhani <suhaninegi124@gmail.com>
2025-08-30 08:58:25 +0530

    css improved

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 //typed js
-var typed = new Typed('#element', {
-    strings: ['Problem Solver','Web Developer','Tech Enthusiast','Team Player','Learner'],
-    typeSpeed: 60, 
-    loop: true,
+var typed = new Typed("#element", {
+  strings: [
+    "Problem Solver",
+    "Web Developer",
+    "Tech Enthusiast",
+    "Team Player",
+    "Learner",
+  ],
+  typeSpeed: 60,
+  loop: true,
 });
 
+//theme
+const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-//navbar
-const toggleBtn = document.querySelector(".navbar-toggle");
-const navList = document.querySelector(".navlist");
-
-toggleBtn.addEventListener("click", () => {
-  navList.classList.toggle("active");
-});
-
-
-const theme=document.getElementById("theme-toggle");
-const body=document.body;
-
-let isLight=true;
+let isLight = true;
+const theme = document.getElementById("theme-toggle");
+const body = document.body;
 
 theme.addEventListener("click", () => {
   // Fade out icon
@@ -48,79 +46,84 @@ theme.addEventListener("click", () => {
   }, 150);
 });
 
-window.addEventListener("scroll",function(){
-    const header=document.querySelector("header");
-    if(window.scrollY>10){
-        header.classList.add("scrolled")
-    }else{
-        header.classList.remove("scrolled");
-    }
-})
+//navbar
+const toggleBtn = document.querySelector(".navbar-toggle");
+const navList = document.querySelector(".navlist");
+
+toggleBtn.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 10) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
 
 //when box enter viewport
-const observer=new IntersectionObserver((entries)=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){  
-            entry.target.classList.add('show');
-        } else {
-        entry.target.classList.remove('show');  // Remove class when out of view
-        }
-    })
-})
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show"); // Remove class when out of view
+    }
+  });
+});
 
-const hero_button=document.querySelector('.button-container');
+const hero_button = document.querySelector(".button-container");
 observer.observe(hero_button);
 
-const skill_box=document.querySelectorAll('.skill-box');
-skill_box.forEach(box => {
-    observer.observe(box);
+const skill_box = document.querySelectorAll(".skill-box");
+skill_box.forEach((box) => {
+  observer.observe(box);
 });
 
+const skill_box2 = document.querySelectorAll(".card-container");
 
-
-const skill_box2=document.querySelectorAll('.card-container');
-
-skill_box2.forEach(box=>{
-    observer.observe(box)
-})
-
-const gears=document.querySelectorAll('.gear');
-gears.forEach(gear=>{
-    observer.observe(gear)
-})
-
-
-const projectCard=document.querySelectorAll('.project-box');
-projectCard.forEach(card=>{
-    observer.observe(card)
-})
-
-
-const certificates=document.querySelectorAll('.certificate-card');
-certificates.forEach(cert => {
-    observer.observe(cert);
+skill_box2.forEach((box) => {
+  observer.observe(box);
 });
 
+const gears = document.querySelectorAll(".gear");
+gears.forEach((gear) => {
+  observer.observe(gear);
+});
 
+const projectCard = document.querySelectorAll(".project-box");
+projectCard.forEach((card) => {
+  observer.observe(card);
+});
+
+const certificates = document.querySelectorAll(".certificate-card");
+certificates.forEach((cert) => {
+  observer.observe(cert);
+});
 
 //form
-emailjs.init("-R3S2gW4k0cwCQd_V");  //public key of Email js
+emailjs.init("-R3S2gW4k0cwCQd_V"); //public key of Email js
 
-const btn=document.getElementById('sendForm')
+const btn = document.getElementById("sendForm");
 
 //form submission
-document.getElementById('contact-form-data').addEventListener('submit',function(event){
+document
+  .getElementById("contact-form-data")
+  .addEventListener("submit", function (event) {
     event.preventDefault(); //prevent page refresh
 
-    btn.value='Sending..'
-    const serviceId='service_do9olwy';
-    const templateId='template_34ibeem'
+    btn.value = "Sending..";
+    const serviceId = "service_do9olwy";
+    const templateId = "template_34ibeem";
 
-    emailjs.sendForm(serviceId,templateId,this).then(()=>{
-        alert("Sent")
-    },(error)=>{
-        alert("Failed")
-    });
-
-});
-
+    emailjs.sendForm(serviceId, templateId, this).then(
+      () => {
+        alert("Sent");
+      },
+      (error) => {
+        alert("Failed");
+      }
+    );
+  });
